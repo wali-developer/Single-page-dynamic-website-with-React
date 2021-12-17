574cd8decac4e7ceeaaf10ca45926bc4ec24e640
waliullah <waliullah@gmail.com>
2021-12-17 09:39:57 +0500

    Animation changing

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import UseApp from "../components/UseApp";
+import UseApp2 from "../components/useApp2";
 
 const About = () => {
   return (
     <>
       <Navbar />
       <UseApp />
+      <UseApp2 />
       <Footer />
     </>
   );
